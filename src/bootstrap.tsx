efcb59d4f42ac76e77d56d3e358e15350c196f97
Mamoon <mamoun.abdullah95@gmail.com>
2026-07-19 02:15:11 +0300

    perf: mount shared notification provider

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -5,15 +5,18 @@ import "./i18n";
 import { AppV2 } from "./AppV2";
 import { AuthProvider } from "./contexts/AuthContext";
 import { TaxonomyProvider } from "./contexts/TaxonomyContext";
+import { NotificationProvider } from "./contexts/NotificationContext";
 
 export function renderApplication(rootElement: HTMLElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <BrowserRouter>
         <AuthProvider>
-          <TaxonomyProvider>
-            <AppV2 />
-          </TaxonomyProvider>
+          <NotificationProvider>
+            <TaxonomyProvider>
+              <AppV2 />
+            </TaxonomyProvider>
+          </NotificationProvider>
         </AuthProvider>
       </BrowserRouter>
     </React.StrictMode>,
